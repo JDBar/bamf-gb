16bbb09a1b3fea83f8b873df5c0b6d806b546bbf
JDBar <jdbar1215@gmail.com>
2018-10-28 14:16:46 -0700

    Switch case to map

--- src/classes/class.MMU.ts
+++ src/classes/class.MMU.ts
@@ -46,6 +46,8 @@ export default class MMU {
    */
   private zeroPageRAM: Uint8Array; // 128 B
 
+  private map: IAddressHashMap;
+
   constructor() {
     this.inBIOS = true;
     this.BIOS = new Uint8Array(256);
@@ -53,6 +55,28 @@ export default class MMU {
     this.workingRAM = new Uint8Array(8192);
     this.externalRAM = new Uint8Array(8192);
     this.zeroPageRAM = new Uint8Array(128);
+
+    /**
+     * A map of address spaces to handler functions.
+     */
+    this.map = {
+      0x0000: this.handle0000,
+      0x1000: this.handle1000,
+      0x2000: this.handle1000,
+      0x3000: this.handle1000,
+      0x4000: this.handle4000,
+      0x5000: this.handle4000,
+      0x6000: this.handle4000,
+      0x7000: this.handle4000,
+      0x8000: this.handle8000,
+      0x9000: this.handle8000,
+      0xa000: this.handleA000,
+      0xb000: this.handleA000,
+      0xc000: this.handleC000,
+      0xd000: this.handleC000,
+      0xe000: this.handleC000,
+      0xf000: this.handleF000,
+    };
   }
 
   /**
@@ -61,99 +85,13 @@ export default class MMU {
    * @param address
    */
   public readByte(address: number): number {
-    switch (address & 0xf000) {
-      /**
-       * BIOS (256 B) || ROM0
-       */
-      case 0x0000:
-        return this.inBIOS && address < 0x0100
-          ? this.BIOS[address]
-          : this.ROM[address];
-
-      /**
-       * ROM0
-       */
-      case 0x1000:
-      case 0x2000:
-      case 0x3000:
-        return this.ROM[address];
-
-      /**
-       * ROM1 (unbanked) 16 KiB
-       */
-      case 0x4000:
-      case 0x5000:
-      case 0x6000:
-      case 0x7000:
-        return this.ROM[address];
-
-      /**
-       * Graphics: VRAM (8 KiB)
-       */
-      case 0x8000:
-      case 0x9000:
-        throw new Error("VRAM not implemented.");
-
-      /**
-       * External RAM (8 KiB)
-       */
-      case 0xa000:
-      case 0xb000:
-        return this.externalRAM[address & 0x1fff];
-
-      /**
-       * Working RAM (8 KiB)
-       */
-      case 0xc000:
-      case 0xd000:
-        return this.workingRAM[address & 0x1fff];
-
-      /**
-       * Working RAM shadow
-       */
-      case 0xe000:
-        return this.workingRAM[address & 0x1fff];
-
-      /**
-       * Working RAM shadow, I/O, Zero-page RAM
-       */
-      case 0xf000:
-        switch (address & 0x0f00) {
-          // Working RAM shadow.
-          case 0x000:
-          case 0x100:
-          case 0x200:
-          case 0x300:
-          case 0x400:
-          case 0x500:
-          case 0x600:
-          case 0x700:
-          case 0x800:
-          case 0x900:
-          case 0xa00:
-          case 0xb00:
-          case 0xc00:
-          case 0xd00:
-            return this.workingRAM[address & 0x1fff];
-
-          // Graphics: Object Attribute Memory
-          // OAM is 160 bytes, remaining bytes read as 0.
-          case 0xe00:
-            if (address < 0xfea0) {
-              throw new Error("GPU OAM not implemented.");
-            }
-            return 0;
-
-          case 0xf00:
-            if (address >= 0xff80) {
-              return this.zeroPageRAM[address & 0x7f];
-            }
-            throw new Error("I/O Control not implemented.");
-        }
-        break;
+    var result;
+    address &= 0xffff;
+    result = this.map[address & 0xf000](address);
+    if (result == null) {
+      throw new Error("Unexpected non-number value from read.");
     }
-
-    return 0;
+    return result;
   }
 
   /**
@@ -162,6 +100,137 @@ export default class MMU {
    * @param address
    */
   public readWord(address: number): number {
-    return this.readByte(address) + (this.readByte(address) << 8);
+    address &= 0xffff;
+    return this.readByte(address) + (this.readByte(address + 1) << 8);
   }
+
+  /**
+   * Writes a byte to memory.
+   *
+   * @param address
+   * @param writeValue
+   */
+  public writeByte(address: number, writeValue: number): void {
+    writeValue &= 0xff;
+    this.map[address & 0xf000](address, writeValue);
+  }
+
+  /**
+   * Writes a 16-bit word.
+   *
+   * @param address
+   * @param writeValue
+   */
+  public writeWord(address: number, writeValue: number): void {
+    writeValue &= 0xffff;
+    this.writeByte(address, writeValue & 0xff);
+    this.writeByte(address + 1, writeValue >> 8);
+  }
+
+  /**
+   * BIOS (256 B) || ROM0
+   */
+  private handle0000(address: number, writeValue?: number): number | undefined {
+    const toBIOS = this.inBIOS && address < 0x0100;
+    if (writeValue == null) {
+      return toBIOS ? this.BIOS[address] : this.ROM[address];
+    } else if (toBIOS) {
+      this.BIOS[address] = writeValue;
+    } else {
+      this.ROM[address] = writeValue;
+    }
+  }
+
+  /**
+   * ROM0
+   */
+  private handle1000(address: number, writeValue?: number): number | undefined {
+    if (writeValue == null) {
+      return this.ROM[address];
+    } else {
+      this.ROM[address] = address;
+    }
+  }
+
+  /**
+   * ROM1 (unbanked) 16 KiB
+   */
+  private handle4000(address: number, writeValue?: number): number | undefined {
+    if (writeValue == null) {
+      return this.ROM[address];
+    } else {
+      this.ROM[address] = address;
+    }
+  }
+
+  /**
+   * Graphics: VRAM (8 KiB)
+   */
+  private handle8000(address: number, writeValue?: number): number | undefined {
+    throw new Error("VRAM not implemented.");
+  }
+
+  /**
+   * External RAM (8 KiB)
+   */
+  private handleA000(address: number, writeValue?: number): number | undefined {
+    if (writeValue == null) {
+      return this.externalRAM[address & 0x1fff];
+    } else {
+      this.externalRAM[address & 0x1fff] = writeValue;
+    }
+  }
+
+  /**
+   * Working RAM (8 KiB)
+   * Working RAM Shadow
+   */
+  private handleC000(address: number, writeValue?: number): number | undefined {
+    if (writeValue == null) {
+      return this.workingRAM[address & 0x1fff];
+    } else {
+      this.workingRAM[address & 0x1fff] = writeValue;
+    }
+  }
+
+  /**
+   * Working RAM shadow, OAM, Zero-page RAM, I/O
+   */
+  private handleF000(address: number, writeValue?: number): number | undefined {
+    const address2 = address & 0x0f00;
+    if (address2 <= 0xd00) {
+      // Working RAM Shadow
+      if (writeValue == null) {
+        return this.workingRAM[address & 0x1fff];
+      } else {
+        this.workingRAM[address & 0x1fff] = writeValue;
+      }
+    } else if (address2 === 0xe00) {
+      if (address < 0xfea0) {
+        // GPU Object Attribute Memory
+        throw new Error("GPU OAM not implemented.");
+      } else {
+        // 0xFEA0 - 0xFEFF is unused.
+        if (writeValue == null) {
+          return 0;
+        }
+      }
+    } else {
+      if (address >= 0xff80) {
+        // Zero-page RAM
+        if (writeValue == null) {
+          return this.zeroPageRAM[address & 0x7f];
+        } else {
+          this.zeroPageRAM[address & 0x7f] = writeValue;
+        }
+      } else {
+        // I/O
+        throw new Error("I/O Control not implemented.");
+      }
+    }
+  }
+}
+
+interface IAddressHashMap {
+  [key: number]: (address: number, writeValue?: number) => number | undefined;
 }
